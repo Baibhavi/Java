2ad2484704e98bd0b8aab4bd928ea6992103eb85
Baibhavi Pandey <100146898+Baibhavi@users.noreply.github.com>
2022-08-12 23:21:09 +0530

    Add files via upload

---
+++ Methods/helloWorld.java
@@ -0,0 +1,9 @@
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World !");
+        System.out.println("I am learning Java from scratch.");
+        System.out.println("Hope I'll do better.");
+        System.out.println("Hello World!\nPrepinsta is Awesome\nWelcome to the Java course\nIf you want to learn Java in depth, complete this playlist.");
+        
+    }
+}
