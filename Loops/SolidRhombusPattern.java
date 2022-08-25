defc6017eded1de84f192676965fd2ae0f88946f
Baibhavi Pandey <100146898+Baibhavi@users.noreply.github.com>
2022-08-25 21:38:14 +0530

    Add files via upload

--- Loops/SolidRhombusPattern.java
+++ Loops/SolidRhombusPattern.java
@@ -13,5 +13,15 @@ public class SolidRhombusPattern {
             }
             System.out.println();
         }
-    }
+        //or
+        for(int i=1;i<=n;i++){
+            for(int j=n-i;j>=1;j--){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=n;j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+
+         } }
 }
